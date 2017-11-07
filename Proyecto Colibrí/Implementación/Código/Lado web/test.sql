90003779990ec484c0531e60d18f87c38d4db299
Ariel Machini <arielmachini@protonmail.com>
2017-11-07 09:43:53 -0300

    ¡Implementación parcial del ABM de gestores de formularios exitosa!

---
+++ Proyecto Colibrí/Implementación/Código/Lado web/test.sql
@@ -0,0 +1,10 @@
+USE uargflow;
+SELECT * FROM GESTOR_FORMULARIOS;
+
+UPDATE uargflow.USUARIO_ROL
+SET idrol=1
+WHERE idusuario=2;
+
+UPDATE uargflow.USUARIO_ROL
+SET idrol=1
+WHERE idusuario=4;
---
+++ Proyecto Colibrí/Implementación/Código/Lado web/uargflow/colibri/colibri.js
@@ -0,0 +1,3 @@
+function abrirDialogoBusquedaAlta() {
+    window.open("alta_gestor_busqueda.php", "_blank", "toolbar=no, scrollbars=yes, resizable=no, top=75, left=100, width=800, height=600");
+}
